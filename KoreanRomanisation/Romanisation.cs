856f83600cbebd89c08cb841582cd7ab9990757c
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2015-11-14 01:53:34 +0000

    Base romanisation

---
+++ KoreanRomanisation/Romanisation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoreanRomanisation
+{
+    public abstract class Romanisation
+    {
+        protected int FirstCode = 44032;
+        protected int LastCode = 55203;
+
+        protected int NumberOfInitialJamo = 19;
+        protected int NumberOfMedialJamo = 21;
+        protected int NumberOfFinalJamo = 28;
+
+        public int[] JamoCodes(int CharacterCode)
+        {
+            if (CharacterCode >= FirstCode && CharacterCode <= LastCode)
+            {
+                int InitialJamoCode = 0;
+                int MedialJamoCode = 0;
+                int FinalJamoCode = 0;
+
+                int CodeOffset = CharacterCode - FirstCode;
+
+                InitialJamoCode = CodeOffset / (NumberOfMedialJamo * NumberOfFinalJamo);
+                CodeOffset = CodeOffset % (NumberOfMedialJamo * NumberOfFinalJamo);
+
+                MedialJamoCode = CodeOffset / NumberOfFinalJamo;
+                CodeOffset = CodeOffset % NumberOfFinalJamo;
+
+                FinalJamoCode = CodeOffset;
+
+                return new int[] { InitialJamoCode, MedialJamoCode, FinalJamoCode };
+            }
+
+            return null;
+        }
+    }
+}
---
+++ KoreanRomanisation/YaleRomanisation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoreanRomanisation
+{
+    public class YaleRomanisation : IRomanisation
+    {
+        public string Romanise(string Korean)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
