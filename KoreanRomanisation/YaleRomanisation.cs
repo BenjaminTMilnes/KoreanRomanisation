b083e291ba9f35a40d24fed308d1f548fffd7e6f
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2017-10-21 18:00:51 +0100

    Simplified YaleRomanisation

--- KoreanRomanisation/YaleRomanisation.cs
+++ KoreanRomanisation/YaleRomanisation.cs
@@ -6,6 +6,18 @@ using System.Threading.Tasks;
 
 namespace KoreanRomanisation
 {
+    public class TupleList<T1, T2> : List<Tuple<T1, T2>>
+    {
+        public void Add(T1 Item1, T2 Item2)
+        {
+            Add(new Tuple<T1, T2>(Item1, Item2));
+        }
+    }
+
+    public class RomanisationRuleList : TupleList<KoreanLetter, string>
+    {
+    }
+
     public class YaleRomanisation : Romanisation
     {
         private InitialRomanisationRule[] InitialRomanisationRules;
@@ -14,81 +26,85 @@ namespace KoreanRomanisation
 
         public YaleRomanisation()
         {
-            InitialRomanisationRules = new InitialRomanisationRule[] {
-                new InitialRomanisationRule(KoreanLetter.Giyeok, "k"),
-                new InitialRomanisationRule(KoreanLetter.SsangGiyeok, "kk"),
-                new InitialRomanisationRule(KoreanLetter.Nieun, "n"),
-                new InitialRomanisationRule(KoreanLetter.Digeut, "t"),
-                new InitialRomanisationRule(KoreanLetter.SsangDigeut, "tt"),
-                new InitialRomanisationRule(KoreanLetter.Rieul, "l"),
-                new InitialRomanisationRule(KoreanLetter.Mieum, "m"),
-                new InitialRomanisationRule(KoreanLetter.Bieup, "p"),
-                new InitialRomanisationRule(KoreanLetter.SsangBieup, "pp"),
-                new InitialRomanisationRule(KoreanLetter.Shiot, "s"),
-                new InitialRomanisationRule(KoreanLetter.SsangShiot, "ss"),
-                new InitialRomanisationRule(KoreanLetter.Ieung, ""),
-                new InitialRomanisationRule(KoreanLetter.Jieut, "c"),
-                new InitialRomanisationRule(KoreanLetter.SsangJieut, "cc"),
-                new InitialRomanisationRule(KoreanLetter.Chieut, "ch"),
-                new InitialRomanisationRule(KoreanLetter.Kieuk, "kh"),
-                new InitialRomanisationRule(KoreanLetter.Tieut, "th"),
-                new InitialRomanisationRule(KoreanLetter.Pieup, "ph"),
-                new InitialRomanisationRule(KoreanLetter.Hieut, "h")
+            var InitialRomanisationRulesList = new RomanisationRuleList() {
+                { KoreanLetter.Giyeok, "k" },
+                { KoreanLetter.SsangGiyeok, "kk" },
+                { KoreanLetter.Nieun, "n" },
+                { KoreanLetter.Digeut, "t" },
+                { KoreanLetter.SsangDigeut, "tt" },
+                { KoreanLetter.Rieul, "l" },
+                { KoreanLetter.Mieum, "m" },
+                { KoreanLetter.Bieup, "p" },
+                { KoreanLetter.SsangBieup, "pp" },
+                { KoreanLetter.Shiot, "s" },
+                { KoreanLetter.SsangShiot, "ss" },
+                { KoreanLetter.Ieung, "" },
+                { KoreanLetter.Jieut, "c" },
+                { KoreanLetter.SsangJieut, "cc" },
+                { KoreanLetter.Chieut, "ch" },
+                { KoreanLetter.Kieuk, "kh" },
+                { KoreanLetter.Tieut, "th" },
+                { KoreanLetter.Pieup, "ph" },
+                { KoreanLetter.Hieut, "h" }
             };
 
-            MedialRomanisationRules = new MedialRomanisationRule[] {
-                new MedialRomanisationRule(KoreanLetter.A, "a"),
-                new MedialRomanisationRule(KoreanLetter.Ae, "ay"),
-                new MedialRomanisationRule(KoreanLetter.Ya, "ya"),
-                new MedialRomanisationRule(KoreanLetter.Yae, "yay"),
-                new MedialRomanisationRule(KoreanLetter.Eo, "e"),
-                new MedialRomanisationRule(KoreanLetter.E, "ey"),
-                new MedialRomanisationRule(KoreanLetter.Yeo, "ye"),
-                new MedialRomanisationRule(KoreanLetter.Ye, "yey"),
-                new MedialRomanisationRule(KoreanLetter.O, "o"),
-                new MedialRomanisationRule(KoreanLetter.Wa, "wa"),
-                new MedialRomanisationRule(KoreanLetter.Wae, "way"),
-                new MedialRomanisationRule(KoreanLetter.Oe, "oy"),
-                new MedialRomanisationRule(KoreanLetter.Yo, "yo"),
-                new MedialRomanisationRule(KoreanLetter.U, "u"),
-                new MedialRomanisationRule(KoreanLetter.Wo, "we"),
-                new MedialRomanisationRule(KoreanLetter.We, "wey"),
-                new MedialRomanisationRule(KoreanLetter.Wi, "wi"),
-                new MedialRomanisationRule(KoreanLetter.Yu, "yu"),
-                new MedialRomanisationRule(KoreanLetter.Eu, "u"),
-                new MedialRomanisationRule(KoreanLetter.Ui, "uy"),
-                new MedialRomanisationRule(KoreanLetter.I, "i")
+            var MedialRomanisationRulesList = new RomanisationRuleList(){
+                {KoreanLetter.A, "a"},
+                {KoreanLetter.Ae, "ay"},
+                {KoreanLetter.Ya, "ya"},
+                {KoreanLetter.Yae, "yay"},
+                {KoreanLetter.Eo, "e"},
+                {KoreanLetter.E, "ey"},
+                {KoreanLetter.Yeo, "ye"},
+                {KoreanLetter.Ye, "yey"},
+                {KoreanLetter.O, "o"},
+                {KoreanLetter.Wa, "wa"},
+                {KoreanLetter.Wae, "way"},
+                {KoreanLetter.Oe, "oy"},
+                {KoreanLetter.Yo, "yo"},
+                {KoreanLetter.U, "u"},
+                {KoreanLetter.Wo, "we"},
+                {KoreanLetter.We, "wey"},
+                {KoreanLetter.Wi, "wi"},
+                {KoreanLetter.Yu, "yu"},
+                {KoreanLetter.Eu, "u"},
+                {KoreanLetter.Ui, "uy"},
+                {KoreanLetter.I, "i" }
             };
 
-            FinalRomanisationRules = new FinalRomanisationRule[] {
-                new FinalRomanisationRule(KoreanLetter.GiyeokBatchim, "k"),
-                new FinalRomanisationRule(KoreanLetter.SsangGiyeokBatchim, "kk"),
-                new FinalRomanisationRule(KoreanLetter.GiyeokShiotBatchim, "ks"),
-                new FinalRomanisationRule(KoreanLetter.NieunBatchim, "n"),
-                new FinalRomanisationRule(KoreanLetter.NieunJieutBatchim, "nc"),
-                new FinalRomanisationRule(KoreanLetter.NieunHieutBatchim, "nh"),
-                new FinalRomanisationRule(KoreanLetter.DigeutBatchim, "t"),
-                new FinalRomanisationRule(KoreanLetter.RieulBatchim, "l"),
-                new FinalRomanisationRule(KoreanLetter.RieulGiyeokBatchim, "lk"),
-                new FinalRomanisationRule(KoreanLetter.RieulMieumBatchim, "lm"),
-                new FinalRomanisationRule(KoreanLetter.RieulBieupBatchim, "lp"),
-                new FinalRomanisationRule(KoreanLetter.RieulShiotBatchim, "ls"),
-                new FinalRomanisationRule(KoreanLetter.RieulTieutBatchim, "lth"),
-                new FinalRomanisationRule(KoreanLetter.RieulPieupBatchim, "lph"),
-                new FinalRomanisationRule(KoreanLetter.RieulHieutBatchim, "lh"),
-                new FinalRomanisationRule(KoreanLetter.MieumBatchim, "m"),
-                new FinalRomanisationRule(KoreanLetter.BieupBatchim, "p"),
-                new FinalRomanisationRule(KoreanLetter.BieupShiotBatchim, "ps"),
-                new FinalRomanisationRule(KoreanLetter.ShiotBatchim, "s"),
-                new FinalRomanisationRule(KoreanLetter.SsangShiotBatchim, "ss"),
-                new FinalRomanisationRule(KoreanLetter.IeungBatchim, "ng"),
-                new FinalRomanisationRule(KoreanLetter.JieutBatchim, "c"),
-                new FinalRomanisationRule(KoreanLetter.ChieutBatchim, "ch"),
-                new FinalRomanisationRule(KoreanLetter.KieukBatchim, "kh"),
-                new FinalRomanisationRule(KoreanLetter.TieutBatchim, "th"),
-                new FinalRomanisationRule(KoreanLetter.PieupBatchim, "ph"),
-                new FinalRomanisationRule(KoreanLetter.HieutBatchim, "h")
+            var FinalRomanisationRulesList = new RomanisationRuleList(){
+                {KoreanLetter.GiyeokBatchim, "k"},
+                {KoreanLetter.SsangGiyeokBatchim, "kk"},
+                {KoreanLetter.GiyeokShiotBatchim, "ks"},
+                {KoreanLetter.NieunBatchim, "n"},
+                {KoreanLetter.NieunJieutBatchim, "nc"},
+                {KoreanLetter.NieunHieutBatchim, "nh"},
+                {KoreanLetter.DigeutBatchim, "t"},
+                {KoreanLetter.RieulBatchim, "l"},
+                {KoreanLetter.RieulGiyeokBatchim, "lk"},
+                {KoreanLetter.RieulMieumBatchim, "lm"},
+                {KoreanLetter.RieulBieupBatchim, "lp"},
+                {KoreanLetter.RieulShiotBatchim, "ls"},
+                {KoreanLetter.RieulTieutBatchim, "lth"},
+                {KoreanLetter.RieulPieupBatchim, "lph"},
+                {KoreanLetter.RieulHieutBatchim, "lh"},
+                {KoreanLetter.MieumBatchim, "m"},
+                {KoreanLetter.BieupBatchim, "p"},
+                {KoreanLetter.BieupShiotBatchim, "ps"},
+                {KoreanLetter.ShiotBatchim, "s"},
+                {KoreanLetter.SsangShiotBatchim, "ss"},
+                {KoreanLetter.IeungBatchim, "ng"},
+                {KoreanLetter.JieutBatchim, "c"},
+                {KoreanLetter.ChieutBatchim, "ch"},
+                {KoreanLetter.KieukBatchim, "kh"},
+                {KoreanLetter.TieutBatchim, "th"},
+                {KoreanLetter.PieupBatchim, "ph"},
+                {KoreanLetter.HieutBatchim, "h" }
             };
+
+            InitialRomanisationRules = InitialRomanisationRulesList.Select(t => new InitialRomanisationRule(t.Item1, t.Item2)).ToArray();
+            MedialRomanisationRules = MedialRomanisationRulesList.Select(t => new MedialRomanisationRule(t.Item1, t.Item2)).ToArray();
+            FinalRomanisationRules = FinalRomanisationRulesList.Select(t => new FinalRomanisationRule(t.Item1, t.Item2)).ToArray();
         }
 
         public override string Romanise(KoreanSyllable Syllable1, KoreanSyllable? PrecedingSyllable = null, KoreanSyllable? SucceedingSyllable = null)
